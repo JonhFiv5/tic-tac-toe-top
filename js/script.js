524ebcfd83b8029449c436f09a639ad0ee3e0faa
João Kaíque <joaok231@gmail.com>
2023-12-14 13:53:09 -0300

    Initial implementation of gameboard and game

---
+++ js/script.js
@@ -0,0 +1,98 @@
+function createGameboard() {
+    const board = [];
+    const DEFAULT_CELL_VALUE = null;
+
+    const generateBoard = () => {
+        for (let i = 0; i < 3; i++) {
+            board.push([
+                DEFAULT_CELL_VALUE,
+                DEFAULT_CELL_VALUE,
+                DEFAULT_CELL_VALUE,
+            ]);
+        }
+    };
+
+    generateBoard();
+
+    const getGameboard = () => board;
+
+    const assignPlayerMove = (row, column, playerSymbol) => {
+        if (board[row][column] === null) {
+            board[row][column] = playerSymbol;
+        }
+    };
+
+    const verifyVictory = (playerSymbol) => {
+        // Verify diagonal victory
+        const diagOne = [board[0][0], board[1][1], board[2][2]];
+        const diagTwo = [board[0][2], board[1][1], board[2][0]];
+
+        if (
+            diagOne.every((cell) => cell === playerSymbol) ||
+            diagTwo.every((cell) => cell === playerSymbol)
+        ) {
+            return true;
+        }
+
+        // Verify row victory
+        const rowVictory = board.some((row) =>
+            row.every((cell) => cell === playerSymbol)
+        );
+
+        // Verify col victory
+        const COLUMS = [0, 1, 2];
+        const colVictory = COLUMS.some((column) => {
+            return (
+                board[0][column] === playerSymbol &&
+                board[1][column] === playerSymbol &&
+                board[2][column] === playerSymbol
+            );
+        });
+
+        return rowVictory || colVictory;
+    };
+
+    return { getGameboard, assignPlayerMove, verifyVictory, generateBoard };
+}
+
+function createGame() {
+    const gameboard = createGameboard();
+    const players = [
+        {
+            name: 'player one',
+            symbol: 'X',
+            score: 0,
+        },
+        {
+            name: 'player two',
+            symbol: 'O',
+            score: 0,
+        },
+    ];
+    let currentPlayer = players[0];
+
+    const printGameboard = () => console.table(gameboard.getGameboard());
+
+    const playTurn = (row, column) => {
+        gameboard.assignPlayerMove(row, column, currentPlayer.symbol);
+        printGameboard();
+        const playerWin = gameboard.verifyVictory(currentPlayer.symbol);
+        if (playerWin) {
+            console.log(`${currentPlayer.name} wins!\nStarting a new game...`);
+            currentPlayer.score += 1;
+            gameboard.generateBoard();
+        } else {
+            currentPlayer =
+                currentPlayer.name === 'player one' ? players[1] : players[0];
+        }
+    };
+
+    const printCurrentScore = () =>
+        console.log(
+            `Player one: ${players[0].score}\nPlayer two: ${players[1].score}`
+        );
+
+    return { playTurn, printGameboard, printCurrentScore };
+}
+
+const game = createGame();
